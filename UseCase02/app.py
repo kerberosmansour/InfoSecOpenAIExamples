4ff1f39b720d1a43a95cb03a1cb92f374762d4ba
kerberosmansour <cherifmansour@gmail.com>
2023-05-06 17:51:56 +0100

    Revert "Moved to chatgpt from gpt-3"
    This reverts commit eb7e4733d1bd0c4ab336d0e0ccbd9529f9712dda.

--- UseCase02/app.py
+++ UseCase02/app.py
@@ -14,11 +14,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 CORS(app)
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_directory)
-
 df = pd.read_parquet('url_contents_embedded.parquet')
-
 # Read the data from your DataFrame
 embeddings = np.stack(df["ada_v2_embedding"].to_numpy())
 
@@ -56,7 +52,7 @@ def generate_text():
     ]
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        engine="gpt-35-turbo",
         messages=messages,
     )
     
