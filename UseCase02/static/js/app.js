1ef4816af733217ca246bb96a7350e041620f268
kerberosmansour <cherifmansour@gmail.com>
2023-05-06 15:23:33 +0100

    style changes
    style changes

--- UseCase02/static/js/app.js
+++ UseCase02/static/js/app.js
@@ -1,140 +1,156 @@
-class DataTable extends React.Component {
-  render() {
-    const { data } = this.props;
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-    if (!data || data.length === 0) {
-      return null;
-    }
+const DataTable = ({ data }) => {
+  if (!data || data.length === 0) {
+    return null;
+  }
 
-    const header = Object.keys(data[0]);
+  const header = Object.keys(data[0]);
 
-    return (
-      <table className="table table-bordered mt-2">
-        <thead>
-          <tr>
-            {header.map((key, index) => (
-              <th key={index}>{key}</th>
+  return (
+    <table className="table table-bordered mt-2">
+      <thead>
+        <tr>
+          {header.map((key, index) => (
+            <th key={index}>{key}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {Object.values(row).map((value, index) => (
+              <td key={index}>{value}</td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {data.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {Object.values(row).map((value, index) => (
-                <td key={index}>{value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const parseResponse = (response) => {
+  const codeRegex = /```([\s\S]*?)```/g;
+  const match = codeRegex.exec(response);
+  if (match) {
+    const code = match[1];
+    return (
+      <pre>
+        <code className="language-javascript">{code}</code>
+      </pre>
     );
   }
-}
+  return response;
+};
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      prompt: "",
-      responses: [],
-      isLoading: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const App = () => {
+  const [prompt, setPrompt] = useState('');
+  const [responses, setResponses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  componentDidMount() {
+  useEffect(() => {
     hljs.highlightAll();
-  }
+  }, [responses]);
 
-  componentDidUpdate() {
-    hljs.highlightAll();
-  }
+  const handleChange = (event) => {
+    setPrompt(event.target.value);
+  };
 
-  handleChange(event) {
-    this.setState({ prompt: event.target.value });
-  }
-
-  renderCodeSnippet(code) {
-    return <pre><code className="language-javascript">{code}</code></pre>;
-  }
-
-  async handleSubmit(event) {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { prompt } = this.state;
 
-    this.setState({ isLoading: true });
+    setIsLoading(true);
 
     try {
-      const response = await axios.post("/api/generate-text", { prompt });
+      const response = await axios.post('/api/generate-text', { prompt });
       const parser = new DOMParser();
-      const tableHtml = parser.parseFromString(response.data.table, "text/html");
-      const tableData = Array.from(tableHtml.querySelectorAll("table tr")).slice(1).map((tr) => {
-        const rowData = Array.from(tr.children).reduce((rowObj, td, colIndex) => {
-          const key = tableHtml.querySelector(`table tr:first-child th:nth-child(${colIndex + 1})`).textContent.trim();
-          rowObj[key] = td.textContent.trim();
-          return rowObj;
-        }, {});
-        return rowData;
-      });
+      const tableHtml = parser.parseFromString(response.data.table, 'text/html');
+      const tableData = Array.from(tableHtml.querySelectorAll('table tr'))
+        .slice(1)
+        .map((tr) => {
+          const rowData = Array.from(tr.children).reduce((rowObj, td, colIndex) => {
+            const key = tableHtml.querySelector(`table tr:first-child th:nth-child(${colIndex + 1})`).textContent.trim();
+            rowObj[key] = td.textContent.trim();
+            return rowObj;
+          }, {});
+          return rowData;
+        });
 
-      this.setState((prevState) => ({
-        responses: [
-          ...prevState.responses,
-          {
-            prompt,
-            response: response.data.response,
-            table: tableData,
-          },
-        ],
-        prompt: "",
-        isLoading: false,
-      }));
+      setResponses((prevState) => [
+        ...prevState,
+        {
+          prompt,
+          response: response.data.response,
+          table: tableData,
+          showTable: false,
+        },
+      ]);
+      setPrompt('');
+      setIsLoading(false);
     } catch (error) {
-      console.error("Error:", error);
-      this.setState({ isLoading: false });
+      console.error('Error:', error);
+      setIsLoading(false);
     }
-  }
-  render() {
-    const { prompt, responses, isLoading } = this.state;
-  
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="input-group mt-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter your prompt"
-              value={prompt}
-              onChange={this.handleChange}
-            />
-            <button className="btn btn-primary" type="submit" disabled={isLoading}>
-              Submit
-            </button>
-          </div>
-        </form>
-        {isLoading && <div className="mt-3 spinner"></div>}
-        <div className="mt-4">
-          {responses.slice(0).reverse().map((item, index) => (
-            <div key={index} className="mb-3">
-              <div className="alert alert-primary" role="alert">
-                <strong>User:</strong> {item.prompt} <br />
-                <small>{item.timestamp}</small>
-              </div>
-              <div className="alert alert-success" role="alert">
-                {this.renderCodeSnippet(item.response)}
-              </div>
-              <DataTable data={item.table} />
-            </div>
-          ))}
-        </div>
-      </div>
+  };
+
+  const toggleTable = (index) => {
+    setResponses((prevResponses) =>
+      prevResponses.map((response, i) => {
+        if (i === index) {
+          return { ...response, showTable: !response.showTable };
+        }
+        return response;
+      })
     );
-  }
-  
+  };
 
-}
+  return (
+    <div>
+      {/* Form to submit prompts */}
+      <form onSubmit={handleSubmit}>
+        <div className="input-group mt-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your prompt"
+            value={prompt}
+            onChange={handleChange}
+          />
+          <button className="btn btn-primary" type="submit" disabled={isLoading}>
+            Submit
+          </button>
+        </div>
+      </form>
+      {/* Loading spinner */}
+      {isLoading && <div className="mt-3 spinner"></div>}
+      {/* List of responses */}
+      <div className="mt-4">
+        {responses.slice(0).reverse().map((item, index) => (
+          <div key={index} className="mb-3">
+            <div className="alert alert-primary" role="alert">
+              <strong>User:</strong> {item.prompt} <br />
+              <small>{item.timestamp}</small>
+            </div>
+            <div className="alert alert-success" role="alert">
+              {parseResponse(item.response)}
+            </div>
+            {/* Toggle table button */}
+            <button
+              className="btn btn-link p-0"
+              onClick={() => toggleTable(index)}
+            >
+              {item.showTable ? "Hide table" : "Show table"}
+            </button>
+            {/* Conditional rendering of the table */}
+            {item.showTable && <DataTable data={item.table} />}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
+ReactDOM.render(<App />, document.getElementById('app'));
 
-ReactDOM.render(<App />, document.getElementById("app"));
